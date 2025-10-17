1c806a3ec211d240d9a869473e78720a75fd3f70
linvict321 <linvict321@gmail.com>
2025-10-16 20:28:13 -0700

    update img

--- src/blog.js
+++ src/blog.js
@@ -1,1 +1,11 @@
 var blogContainer = document.getElementById('blog-container');
+var blog = [
+    {
+        title: "Joe",
+        date: "Mama",
+        description: "06-19-2000",
+        image: "sky.png",
+        imageAlt: "imageAlt",
+        slug: "blog-ts",
+    }
+];
--- src/blog.ts
+++ src/blog.ts
@@ -1,1 +1,21 @@
 const blogContainer = document.getElementById('blog-container');
+
+type blogContainer = {
+    title: String;
+    date : String;
+    description: string;
+    image: string; 
+    imageAlt: string;
+    slug: string;
+}
+
+const blog: blogContainer[] = [
+  {
+    title: "Joe",
+    date: "Mama",
+    description: "06-19-2000",
+    image: "sky.png",
+    imageAlt: "imageAlt",
+    slug: "understanding-typescript",
+  }
+]
