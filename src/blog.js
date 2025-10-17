b3855d04523ae113e12b8a9f40680b3de0d37d84
linvict321 <linvict321@gmail.com>
2025-10-16 21:20:41 -0700

    Added apple1 and apple2 blog pages

--- src/blog.js
+++ src/blog.js
@@ -1,11 +1,38 @@
-var blogContainer = document.getElementById('blog-container');
+//const blogContainer = document.getElementById('blog-container');
 var blog = [
     {
-        title: "Joe",
-        date: "Mama",
-        description: "06-19-2000",
-        image: "sky.png",
+        title: "Blog",
+        date: "10-16-2025",
+        description: "blog",
+        image: "apple1.jpg",
         imageAlt: "imageAlt",
-        slug: "blog-ts",
+        slug: "blog/apple1.html",
+    },
+    {
+        title: "Blog2",
+        date: "10-16-2025",
+        description: "blog2",
+        image: "apple2.jpg",
+        imageAlt: "imageAlt",
+        slug: "blog/apple2.html",
     }
 ];
+var blogContainer = document.getElementById("blog-container");
+blog.forEach(function (blog) {
+    var blogDiv = document.createElement("div");
+    blogDiv.classList.add("blog");
+    var image = document.createElement("img");
+    image.src = blog.image;
+    image.alt = blog.imageAlt;
+    image.style.width = '200px'; // 
+    image.style.height = 'auto';
+    var title = document.createElement("h2");
+    title.textContent = blog.title;
+    var description = document.createElement("p");
+    description.textContent = blog.description;
+    var link = document.createElement("a");
+    link.href = blog.slug;
+    link.textContent = "Read More";
+    blogDiv.append(image, title, description, link);
+    blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(blogDiv);
+});
--- src/blog.ts
+++ src/blog.ts
@@ -1,21 +1,56 @@
-const blogContainer = document.getElementById('blog-container');
+//const blogContainer = document.getElementById('blog-container');
 
-type blogContainer = {
-    title: String;
-    date : String;
+type Blog = {
+    title: string;
+    date : string;
     description: string;
     image: string; 
     imageAlt: string;
     slug: string;
 }
 
-const blog: blogContainer[] = [
+const blog: Blog[] = [
   {
-    title: "Joe",
-    date: "Mama",
-    description: "06-19-2000",
-    image: "sky.png",
+    title: "Blog",
+    date: "10-16-2025",
+    description: "blog",
+    image: "apple1.jpg",
     imageAlt: "imageAlt",
-    slug: "understanding-typescript",
+    slug: "blog/apple1.html",
+  },
+
+  {
+    title: "Blog2",
+    date: "10-16-2025",
+    description: "blog2",
+    image: "apple2.jpg",
+    imageAlt: "imageAlt",
+    slug: "blog/apple2.html",
   }
 ]
+
+const blogContainer = document.getElementById("blog-container");
+
+blog.forEach((blog) => {
+  const blogDiv = document.createElement("div");
+  blogDiv.classList.add("blog");
+
+  const image = document.createElement("img");
+  image.src = blog.image;
+  image.alt = blog.imageAlt;
+  image.style.width = '200px'; // 
+  image.style.height = 'auto';
+
+  const title = document.createElement("h2");
+  title.textContent = blog.title;
+
+  const description = document.createElement("p");
+  description.textContent = blog.description;
+
+  const link = document.createElement("a");
+  link.href = blog.slug;
+  link.textContent = "Read More";
+
+  blogDiv.append(image, title, description, link);
+  blogContainer?.appendChild(blogDiv);
+});
